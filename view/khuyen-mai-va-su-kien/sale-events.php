f724bc23ab33d4fb45a6157578cd87bff6ee69d2
TruongDuyPhong22 <151370849+TruongDuyPhong22@users.noreply.github.com>
2023-12-04 21:53:44 +0700

    Update sale-events.php

--- view/khuyen-mai-va-su-kien/sale-events.php
+++ view/khuyen-mai-va-su-kien/sale-events.php
@@ -39,21 +39,24 @@
                         </div>
                     </div>
                     <div class="row tr-movie-active">
+                    <?php foreach ($show_promotion as $row) {
+                            extract($row);
+                         ?>
                         <div class="col-xl-3 col-lg-4 col-sm-6 grid-item grid-sizer cat-two">
                             <div class="movie-item mb-60">
                                 <div class="movie-poster">
-                                    <a href="index.php?act=events_details"><img src="img/poster/ucm_poster01.jpg" alt=""></a>
+                                    <a href="index.php?act=events_details&id_khuyen_mai=<?php echo $row['id_khuyen_mai']; ?>"><img style="width:302.5px; height:429.29px; object-fit:cover;" src="Assets/img/<?php echo $row['img']; ?>" alt=""></a>
                                 </div>
                                 <div class="movie-content">
                                     <div class="top">
-                                        <h5 class="title"><a href="movie-details.html">Women's Day</a></h5>
-                                        <span class="date">2021</span>
+                                        <h5 class="title"><a href="movie-details.html"><?php echo $row['ten_khuyen_mai'] ?></a></h5>
+                                        <!-- <span class="date">2021</span> -->
                                     </div>
                                     <div class="bottom">
                                         <ul>
-                                            <li><span class="quality">hd</span></li>
+                                            <li><span class="quality">Chia sẻ</span></li>
                                             <li>
-                                                <span class="duration"><i class="far fa-clock"></i> 128 min</span>
+                                                <!-- <span class="duration"><i class="far fa-clock"></i> 128 min</span> -->
                                                 <span class="rating"><i class="fas fa-thumbs-up"></i> 3.5</span>
                                             </li>
                                         </ul>
@@ -61,7 +64,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-3 col-lg-4 col-sm-6 grid-item grid-sizer cat-one cat-three">
+                        <?php  } ?>
+                        <!-- <div class="col-xl-3 col-lg-4 col-sm-6 grid-item grid-sizer cat-one cat-three">
                             <div class="movie-item mb-60">
                                 <div class="movie-poster">
                                     <a href="movie-details.html"><img src="img/poster/ucm_poster02.jpg" alt=""></a>
@@ -215,7 +219,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </section>
 
